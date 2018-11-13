08ae1e7067641a2bf46d60cd1995a854f6435bd6
Jagannadha Kishore Paravastu <kishorepj@Jagannadhas-MacBook-Pro.local>
2018-11-13 07:43:36 -0800

    updated logo

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -9,8 +9,8 @@ const Navbar = () => {
     <div className="topbar-left">
       <button className="topbar-toggler">☰</button>
       <Link className="topbar-brand" to="/">
-        <img className="logo-default" src={`${process.env.PUBLIC_URL}/assets/img/logo.png`} alt="logo" />
-        <img className="logo-inverse" src={`${process.env.PUBLIC_URL}/assets/img/logo-light.png`} alt="logo" />
+        <img className="logo-default" src={`${process.env.PUBLIC_URL}/assets/img/kreaas-black.png`} alt="logo" />
+        <img className="logo-inverse" src={`${process.env.PUBLIC_URL}/assets/img/kreaas.png`} alt="logo" />
         </Link>
     </div>
     <div className="topbar-right">
