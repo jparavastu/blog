c7fd0fffb259d7085dbc79eb93105610172728ae
Jagannadha Kishore Paravastu <kishorepj@Jagannadhas-MacBook-Pro.local>
2018-11-13 07:04:57 -0800

    Updated register page

--- src/components/Register/index.jsx
+++ src/components/Register/index.jsx
@@ -1,25 +1,126 @@
 import React from 'react';
+import Axios from 'axios';
 
-const Register = () => {
+import { validateAll } from 'indicative';
 
-    return (
-<div className="mh-fullscreen bg-img center-vh p-20" style={{backgroundImage: 'url(assets/img/bg-girl.jpg)'}}>
+import config from '../../config';
+
+class Register extends React.Component {
+
+  constructor(){
+
+    super();
+
+    this.state = {
+
+      name:'',
+      email:'',
+      password:'',
+      password_confirmation:'',
+      errors: {}
+    }
+  this.handleInputChange = this.handleInputChange.bind(this)
+  }
+
+  handleInputChange(event){
+
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+
+
+const data = this.state;
+const rules = {
+
+  name: 'required|string',
+  email: 'required|email',
+  password: 'required|string|min:6|confirmed'
+};
+
+const messages = {
+
+  required: 'This field is required.',
+  'email.email': 'This email is invalid.',
+  'password.confirmed': 'The password confirmation does not match.'
+};
+
+validateAll(data, rules, messages)
+.then(() => {
+Axios.post(`${config.apiUrl}/auth/register`,{
+  name: this.state.name,
+  email: this.state.email,
+  password: this.state.password
+}).then(response => {
+  this.props.history.push('/');
+}).catch(errors => {
+  console.log(errors.response)
+const formattedErrors = {};
+
+formattedErrors['email'] = errors.response.data['email'][0];
+
+this.setState({
+
+  errors: formattedErrors
+});
+
+})
+})
+.catch(errors => {
+  console.log(errors);
+
+  const formattedErrors = {}
+  errors.forEach(error => formattedErrors[error.field] = error.message)
+  
+  this.setState({
+
+    errors: formattedErrors
+  })
+
+  
+})
+
+  }
+  render () {
+
+    return(
+      <div className="mh-fullscreen bg-img center-vh p-20" style={{backgroundImage: 'url(assets/img/bg-girl.jpg)'}}>
   <div className="card card-shadowed p-50 w-400 mb-0" style={{maxWidth: '100%'}}>
     <h5 className="text-uppercase text-center">Register</h5>
     <br />
     <br />
-    <form className="form-type-material">
+    <form className="form-type-material" onSubmit={this.handleSubmit}>
       <div className="form-group">
-        <input type="text" className="form-control" placeholder="Username" />
+        <input type="text" name="name" onChange={this.handleInputChange} className="form-control" placeholder="Username" />
+        {
+          this.state.errors['name']&&
+          <small className="text-danger">{this.state.errors['name']}</small>
+        }
+        
       </div>
       <div className="form-group">
-        <input type="text" className="form-control" placeholder="Email address" />
+        <input type="text" name="email" onChange={this.handleInputChange} className="form-control" placeholder="Email address" />
+        {
+          this.state.errors['email']&&
+          <small className="text-danger">{this.state.errors['email']}</small>
+        }
       </div>
       <div className="form-group">
-        <input type="password" className="form-control" placeholder="Password" />
+        <input type="password" name="password" onChange={this.handleInputChange} className="form-control" placeholder="Password" />
+        {
+          this.state.errors['password']&&
+          <small className="text-danger">{this.state.errors['password']}</small>
+        }
       </div>
       <div className="form-group">
-        <input type="password" className="form-control" placeholder="Password (confirm)" />
+        <input type="password" name="password_confirmation" onChange={this.handleInputChange} className="form-control" placeholder="Password (confirm)" />
+        {
+          this.state.errors['password_confirmation']&&
+          <small className="text-danger">{this.state.errors['password_confirmation']}</small>
+        }
       </div>
       <br />
       <button className="btn btn-bold btn-block btn-primary" type="submit">Register</button>
@@ -33,6 +134,9 @@ const Register = () => {
 
 
     );
+  }
 }
+
+
 
 export default Register;
